a465e68f5325168106304307eb27db4e1dfd207c
Evgeniy <s.h.a.r.k.112@mail.ru>
2022-12-02 21:42:13 +0700

    solve of task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.Write("Введите число = ");
+int A = int.Parse(Console.ReadLine()!);
+
+if ( A > 99 )
+{
+    int result = A / 100 % 10;
+    Console.WriteLine("Третья цифра числа = " + result );
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет " );
+}
